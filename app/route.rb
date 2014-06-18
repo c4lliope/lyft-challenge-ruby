276bf6746a7d1578e5d61af21a4048739cbe9da8
Grayson Wright <wright.grayson@gmail.com>
2014-06-18 13:27:59 -0400

    Implement simple driving distance

--- app/route.rb
+++ app/route.rb
@@ -1,2 +1,15 @@
+require_relative 'location'
+
 class Route
+  def initialize(locations, map)
+    @locations = locations
+    @map = map
+  end
+
+  def driving_distance
+    map.driving_distance(locations.first, locations.last)
+  end
+
+  private
+  attr_reader :map, :locations
 end
--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -1,8 +1,16 @@
 require_relative '../app/route'
 
 describe Route do
+  let(:pier_39) { Location.new(37.808673, -122.409821) }
+  let(:lombard) { Location.new(37.802139 , -122.41874) }
+
   context 'when defined by two locations' do
-    it 'calculates driving distance'
+    it 'calculates driving distance' do
+      fake_map = double
+      expect(fake_map).to receive(:driving_distance).with(pier_39, lombard) {1.2}
+
+      expect(Route.new([pier_39, lombard], fake_map).driving_distance).to eq 1.2
+    end
   end
 
   context 'when defined by more than two locations' do
